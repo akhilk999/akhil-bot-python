605404fa78fd92f5ddf226b114877012f991b3af
akhilk999 <56555658+akhilk999@users.noreply.github.com>
2023-11-04 23:20:16 +0000

    added new commands

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from pretty_help import DefaultMenu, PrettyHelp
 from discord.ext import commands
 from discord.utils import find
 from replit import db
@@ -31,29 +32,56 @@ client = commands.Bot(command_prefix='&',
 
 #overrides page formatting for MinimalHelpCommand -> puts each page in an embed
 class MyNewHelp(commands.MinimalHelpCommand):
-
   async def send_pages(self):
     destination = self.get_destination()
     for page in self.paginator.pages:
       emby = discord.Embed(description=page)
       await destination.send(embed=emby)
 
-
 client.help_command = MyNewHelp()
 
-
 class MyHelp(commands.HelpCommand):
+  def get_command_signature(self, command):
+    return '%s%s %s' % (self.context.clean_prefix, command.qualified_name, command.signature)
 
+  #overall help command
   async def send_bot_help(self, mapping):
-    embed = discord.Embed(title="Help")
+    embed = discord.Embed(title="Help", color=discord.Color.blurple())
     for cog, commands in mapping.items():
-      command_signatures = [self.get_command_signature(c) for c in commands]
+      filtered = await self.filter_commands(commands, sort=True)
+      command_signatures = [self.get_command_signature(c) for c in filtered]
       if command_signatures:
         cog_name = getattr(cog, "qualified_name", "No Category")
-        #embed.add_field(name=cog_name, value)
+        embed.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
     channel = self.get_destination()
     await channel.send(embed=embed)
 
+  #help for a certain command
+  async def send_command_help(self, command):
+    embed = discord.Embed(title=self.get_command_signature(command), color=discord.Color.random())
+    if command.help:
+      embed.description = command.help
+    if alias := command.aliases:
+      embed.add_field(name="Aliases", value=", ".join(alias), inline=False)
+
+    channel = self.get_destination()
+    await channel.send(embed=embed)
+
+  #helper function to add commands to embed
+  async def send_help_embed(self, title, description, commands):
+    embed = discord.Embed(title=title, description=description or "No help found...")
+    if filtered_commands := await self.filter_commands(commands):
+      for command in filtered_commands:
+        embed.add_field(name=self.get_command_signature(command), value=command.help or "No help found...")
+    await self.get_destination().send(embed=embed)
+    
+  async def send_group_help(self, group):
+    title = self.get_command_signature(group)
+    await self.send_help_embed(title, group.help, group.commands)
+
+  async def send_cog_help(self, cog):
+    title = cog.qualified_name or "No"
+    await self.send_help_embed(f'{title} Category', cog.description, cog.get_commands())
 
 @client.event
 async def on_ready():
